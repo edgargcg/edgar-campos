cb9214716c143a2762306ecdffe1329fceaf8816
Edgar Campos <edgargcg@gmail.com>
2021-04-12 11:23:23 -0500

    webpack update to rerlative path for assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,77 @@
+const fs = require("fs");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
-module.exports = {
-	mode: "development",
-	entry: "./src/index.tsx",
-	output: {
-		path: path.resolve(__dirname, "dist"),
-		filename: "bundle.js",
-	},
-	devServer: {
-		historyApiFallback: true,
-	},
-	resolve: {
-		extensions: [".js", ".jsx", ".ts", ".tsx"],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
-				},
-			},
-			{
-				test: /\.(ts|tsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "ts-loader",
-				},
-			},
-			{
-				test: /\.html$/,
-				use: [
-					{
-						loader: "html-loader",
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
+
+module.exports = (env, args) => {
+	return {
+		mode: args.mode,
+		entry: "./src/index.tsx",
+		output: {
+			path: path.resolve(__dirname, "dist"),
+			filename: "build.js",
+		},
+		devServer: {
+			historyApiFallback: true,
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(js|jsx)$/,
+					exclude: /node_modules/,
+					use: {
+						loader: "babel-loader",
 					},
-				],
+				},
+				{
+					test: /\.(ts|tsx)$/,
+					exclude: /node_modules/,
+					use: {
+						loader: "ts-loader",
+					},
+				},
+				{
+					test: /\.html$/,
+					use: [
+						{
+							loader: "html-loader",
+						},
+					],
+				},
+				{
+					test: /\.(sa|sc|c)ss$/,
+					use: ["style-loader", "css-loader", "sass-loader"],
+				},
+				{
+					test: /\.(png|jpg|gif|svg|woff|woff2|gif|ttf)(\?[a-z0-9=.]+)?$/,
+					use: [
+						{
+							loader: "url-loader",
+							options: {
+								limit: 1,
+							},
+						},
+					],
+				},
+			],
+		},
+		resolve: {
+			modules: ["src", "node_modules"],
+			extensions: [".js", ".jsx", ".ts", ".tsx"],
+			alias: {
+				react: path.resolve(resolveApp("."), "./node_modules/react"),
+				"react-dom": path.resolve(resolveApp("."), "./node_modules/react-dom"),
+				components: path.resolve(resolveApp("src"), "components"),
+				assets: path.resolve(resolveApp("src"), "assets"),
 			},
-			{
-				test: /\.css$/,
-				use: ["style-loader", "css-loader", "sass-loader"],
-			},
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: "./public/index.html",
+				filename: "./index.html",
+			}),
 		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "./public/index.html",
-			filename: "./index.html",
-		}),
-	],
+	};
 };
