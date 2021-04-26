158d7279544aac85b6e318026a51325d30b46ac9
Edgar Campos <edgargcg@gmail.com>
2021-04-26 12:47:14 -0500

    Footer component (#3)
    * Contact section removed from menu
    
    * Footer component

--- src/containers/Home/Index.tsx
+++ src/containers/Home/Index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Experience } from '../../components/Experience'
+import { Footer } from '../../components/Footer'
 
 import { Header } from '../../components/Header'
 import { Profile } from '../../components/Profile'
@@ -8,14 +9,15 @@ import { SocialMedia } from '../../components/SocialMedia'
 import { Technologies } from '../../components/Technologies'
 
 export const Home = () => {
-    return (
-        <>
-            <Header />
-            <Profile />
-            <SocialMedia />
-            <Technologies />
-            <Experience />
-            <Repositories />
-        </>        
-    )
+  return (
+    <>
+      <Header />
+      <Profile />
+      <SocialMedia />
+      <Technologies />
+      <Experience />
+      <Repositories />
+      <Footer />
+    </>
+  )
 }
